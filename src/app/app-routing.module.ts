6e8a98e25c6f476541443cae4275b5645d0cfc1b
Cynthia Teran <al226582@edu.uaa.mx>
2022-04-26 19:08:40 -0500

    Primera Parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { SearchComponent } from './search/search.component';
+import { UnheroeComponent } from './unheroe/unheroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'heroe/:id', component: UnheroeComponent},
+  {path: 'buscador/:nombreh', component: SearchComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
